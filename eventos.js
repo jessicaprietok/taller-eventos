fdd2562d9752cb6173292d657eb6d9600394b87e
Jessica Prieto <jessicapr1202@gmail.com>
2023-08-12 11:49:04 -0300

    Div: diseño y alerta

---
+++ eventos.js
@@ -0,0 +1,9 @@
+var divBoton = document.getElementById("divBoton");
+
+divBoton.style.backgroundColor = "rgb(88, 229, 88)";
+divBoton.style.padding = "5px";
+divBoton.style.textAlign = "center";
+
+divBoton.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
